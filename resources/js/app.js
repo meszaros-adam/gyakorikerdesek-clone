ecc84249593b239e0f365823a556de3d769177c7
Mészáros Ádám <meszike91@gmail.com>
2022-07-18 17:54:31 +0200

    vue configured

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
 import './bootstrap';
+import { createApp } from 'vue'
+// import the root component App from a single-file component.
+import App from './components/app.vue'
+
+const app = createApp(App).mount("#app")
---
+++ resources/js/components/app.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>My first vue3 component edited</h1>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
