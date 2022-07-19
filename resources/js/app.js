5d7fbc58016cfe312db61f552804953f70957797
Mészáros Ádám <meszike91@gmail.com>
2022-07-19 19:17:34 +0200

    navbar

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,9 @@
 import './bootstrap';
 import { createApp } from 'vue'
+import 'bootstrap/dist/css/bootstrap.css'
+
 // import the root component App from a single-file component.
 import App from './components/app.vue'
+
 
 const app = createApp(App).mount("#app")
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,15 +5,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Gyakorikérdések klón</title>
     @vite('resources/css/app.css')
 </head>
 
 <body>
-    <div>laravel</div>
     <div id="app"></div>
-
     @vite('resources/js/app.js')
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 
 </html>
