912af63e7eba26df77fa611f63c03af3c6fcc019
Mészáros Ádám <meszike91@gmail.com>
2022-08-23 21:04:14 +0200

    styling on messages page

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -10,4 +10,10 @@ class Message extends Model
     use HasFactory;
 
     protected $fillable = ['message', 'sender_id', 'addressee_id'];
+
+    protected $with = ["sender"];
+
+    public function sender(){
+        return $this->belongsTo(User::class, 'sender_id' );
+    }
 }
--- resources/js/components/pages/messages.vue
+++ resources/js/components/pages/messages.vue
@@ -6,8 +6,16 @@
             </div>
             <div class="col-md-9">
                 <div class="container bg-dark text-light">
-                    <div v-for="(message, m) in messages" :key="m">
-                        {{ message.message }}
+                    <h1 class="mb-3">Incoming Messages:</h1>
+                    <div class="bg-primary mb-3 p-2 rounded d-flex justify-content-between"
+                        v-for="(message, m) in messages" :key="m">
+                        <div>
+                            <span> {{ message.sender.nickname + ': ' }}</span>
+                            <span> {{ message.message }}</span>
+                        </div>
+                        <div>
+                            <i title="Reply" class="bi bi-reply mx-2 pointer-cursor fs-5"></i>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -23,7 +31,7 @@ import useCallApi from '../composables/useCallApi'
 import { useToast } from 'vue-toastification'
 import sideMenu from "../partials/sideMenu.vue";
 export default {
-    components: {sideMenu},
+    components: { sideMenu },
     setup() {
         const toast = useToast()
         const messages = ref([])
